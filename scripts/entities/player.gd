54035fa1ab26169adbd91205abb43391c649b4c1
Tor Erland Hansen <king.torhansen@gmail.com>
2025-05-07 10:25:05 +0200

    Changed jump to work on a timer-based logic rather than a certain height over jumpstart. This avoids weird height-based nonsense.

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -14,29 +14,32 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Movement vars
 const base_accel : float = 250.0
 const max_speed : float = 100.0
-@export var jump_velocity: float = -350.0
+@export var jump_velocity: float = -300.0
 @export var knockback_reduction = .7
-var jump_y_start : float = 0
-var jump_height_max : float = 50
 var stored_velocity : Vector2 = Vector2.ZERO
+const jumptime = 0.2
+const hangtime = 0.1
 
 # Flags
 var has_double_jumped : bool = false
 var has_landed : bool = false
 #var animation_locked : bool = false
 var in_knockback: bool = false
-var hangtime : bool = false
+var jumping : bool = false
 
 var hearts : int = 3
 
 # Raycast vars
-const RAY_LEN = 60
+const RAY_LEN = 600
 
 func _ready():
 	print("player loaded")
+	$HangTimer.wait_time = hangtime
+	$JumpTimer.wait_time = jumptime
 
 func _physics_process(delta):
-
+	if Input.is_action_just_pressed(&"jump"):
+		_on_jump_button_pressed()
 	# angle to rotate towards
 	var angle := 0.0
 	# Raycasting to detect the angle to the floor
@@ -50,25 +53,18 @@ func _physics_process(delta):
 		var tangent_3d := Vector3(normal.x, normal.y, 0).cross(Vector3.FORWARD)
 		var tangent := Vector2(tangent_3d.x, tangent_3d.y)
 		angle = -normal.angle_to(Vector2.UP)
-		if is_on_floor():
-			# Apply velocity according to surface tangent.
-			# TODO Currently frictionless, we should add friction to limit speed on flat ground.
-			velocity += base_accel * delta * tangent.normalized()
-			velocity.min(Vector2(max_speed, max_speed))
+		# Apply velocity according to surface tangent.
+		# TODO Currently frictionless, we should add friction to limit speed on flat ground.
+		velocity += base_accel * delta * tangent.normalized()
+		velocity.min(Vector2(max_speed, max_speed))
 			
 		#print(str(rad_to_deg(angle)))
-		
+	if not jumping:
+		velocity.y += gravity * delta
 	if is_on_floor():
 		if not anim_sprite.is_playing():
 			anim_sprite.play("skike")
 		anim_sprite.rotation = angle
-	else:
-		# Jump logic
-		if position.y < jump_y_start - jump_height_max:
-			velocity.y = 0
-
-		if not hangtime:
-				velocity.y += gravity * delta
 
 	has_landed = check_if_landing()
 	move_and_slide()
@@ -110,11 +106,14 @@ func _on_health_upgrade_detected(amount):
 func _on_jump_button_pressed():
 	if is_on_floor() and not in_knockback:
 		has_double_jumped = false
-		jump_y_start = position.y
-		$HangTimer.start()
-		hangtime = true
+		$JumpTimer.start()
+		jumping = true
 		anim_sprite.play("jump")
 		velocity.y = jump_velocity
 
+func _on_jump_timer_timeout() -> void:
+	velocity.y = 0
+	$HangTimer.start()
+	
 func _on_hang_timer_timeout() -> void:
-	hangtime = false
+	jumping = false
