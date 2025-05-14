08ab33f663b397e0f73897ec38c1013cdd0ee752
Sr3qt <105977398+Sr3qt@users.noreply.github.com>
2025-05-14 13:32:27 +0200

    Added jump buffering

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -4,6 +4,18 @@ class_name Player
 # Raycast vars
 const RAY_LEN = 600
 
+# Jump buffer
+## The number of frames after leaving a ledge where jumping is still allowed.
+const COYOTE_FRAMES := 3
+## The number of frames early a jump input will still trigger a jump when landing.
+const LANDING_FRAMES := 4
+## The number of consecutive frames the character has been falling for.
+## Is always 0 when on the ground.
+var falling_frames : int
+## How many frames ago we got our newest jump input. Is -1 when no jump has been inputted.
+var jump_frame := -1
+
+
 signal has_died(body)
 signal health_changed(new_health)
 
@@ -32,6 +44,8 @@ var is_skiking := true
 #var animation_locked : bool = false
 var in_knockback: bool = false
 var is_jumping : bool = false
+## Whether the player has jumped in the current coyote frames.
+var has_jumped : bool = false
 var is_alive := true
 
 @onready var health : int = start_health
@@ -45,8 +59,10 @@ func _ready():
 func _physics_process(delta):
 	# Check for player actions
 	if is_alive:
-		if Input.is_action_just_pressed(&"jump"):
-			_on_jump_button_pressed()
+		if Input.is_action_just_pressed("jump"):
+			jump_frame = 0
+
+	jump_check()
 
 	# angle to rotate towards
 	var angle := 0.0
@@ -153,12 +169,40 @@ func _on_knockback_timer_timeout() -> void:
 	# Restore speed but reduced
 	velocity = stored_velocity * knockback_reduction
 
-func _on_jump_button_pressed():
-	if is_on_floor() and not in_knockback:
-		$JumpTimer.start()
-		is_jumping = true
-		anim_sprite.play("jump")
-		velocity.y = jump_velocity
+## Checks if a jump should trigger, if so it triggers a jump.
+func jump_check():
+	# Count the frames we have been in air.
+	if not is_on_floor():
+		falling_frames += 1
+	else:
+		falling_frames = 0
+
+	var is_jump_buffer = jump_frame <= LANDING_FRAMES and is_on_floor()
+	var is_coyote_frames = falling_frames <= COYOTE_FRAMES and not is_on_floor()
+
+	if jump_frame != -1 and (is_jump_buffer or is_coyote_frames) and not has_jumped:
+		if is_jump_buffer and jump_frame != 0:
+			print("You jumped %s frames to early" % [jump_frame])
+		if is_coyote_frames:
+			print("You jumped %s frames to late" % [falling_frames])
+
+		jump()
+		jump_frame = -1
+
+	if jump_frame != -1:
+		jump_frame += 1
+
+	# Reset jump after known safe delay
+	if falling_frames > COYOTE_FRAMES and has_jumped:
+		has_jumped = false
+		jump_frame = -1
+
+func jump():
+	$JumpTimer.start()
+	is_jumping = true
+	has_jumped = true
+	anim_sprite.play("jump")
+	velocity.y = jump_velocity
 
 func _on_jump_timer_timeout() -> void:
 	velocity.y = 0
