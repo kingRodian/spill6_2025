22ac18e0f2ee373e5a920d8261698300b909ff46
Tor Erland Hansen <king.torhansen@gmail.com>
2025-05-22 10:45:43 +0200

    Building stuff

--- scripts/entities/fotgjenger_1.gd
+++ scripts/entities/fotgjenger_1.gd
@@ -4,6 +4,8 @@ const BASE_SPEED : float = 30
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var start_pos : Vector2
 
+# Whether the npc is activated and moves, visibleonscreen turns this on when we can see the pedestrian
+var active := false
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
@@ -15,12 +17,16 @@ func _ready():
 	sprite.play()
 
 func _physics_process(delta):
-	velocity += Vector2.LEFT * BASE_SPEED * delta
-	velocity.y += gravity * delta
-	move_and_slide()
+	if active:
+		velocity += Vector2.LEFT * BASE_SPEED * delta
+		velocity.y += gravity * delta
+		move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 		# Spilleren treffer hinderet
 	print("hit player")
 	if body is Player:
 		body._on_hit(self, body)
+
+func _on_visible_on_screen_enabler_2d_screen_entered() -> void:
+	active = true
